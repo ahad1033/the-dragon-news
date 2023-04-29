3000f96bff7f8bce5cc44adbfb26bba7bdbdb031
ahad1033 <a.ahad2232@gmail.com>
2023-04-30 03:05:12 +0600

    install-marquee

--- src/pages/shared/header/Header.jsx
+++ src/pages/shared/header/Header.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
+import { Button, Container } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
     return (
-        <div>
+        <Container>
             <div className="text-center">
                 <img src={logo} alt="" />
                 <p className='text-secondary'><small>Journalism Without Fear or Favour</small></p>
                 <p>{moment().format("dddd, MMMM D, YYYY")}</p>
             </div>
-        </div>
+            <div>
+                <Button variant="danger">Latest</Button>
+            </div>
+        </Container>
     );
 };
 
