37722ca411eb827c3e5370de0142ab9bb3e73afb
ahad1033 <a.ahad2232@gmail.com>
2023-04-30 02:55:53 +0600

    62-4

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcr2QT488FGylJ6KoMSw-FuK9dWvwDhjg",
+  authDomain: "the-news-dragon-606c9.firebaseapp.com",
+  projectId: "the-news-dragon-606c9",
+  storageBucket: "the-news-dragon-606c9.appspot.com",
+  messagingSenderId: "776643606163",
+  appId: "1:776643606163:web:dfa713dc144631d81b3f66"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/layout/Main.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Header from '../pages/shared/header/Header';
+import Footer from '../pages/shared/footer/Footer';
+import { Col, Container, Row } from 'react-bootstrap';
+import LeftNav from '../pages/shared/LeftNav/LeftNav';
+import RightNav from '../pages/shared/RightNav/RightNav';
+
+const Main = () => {
+    return (
+        <div>
+            <Header />
+            <Container>
+                <Row>
+                    <Col lg={3}>
+                        <LeftNav />
+                    </Col>
+                    <Col lg={6}>
+                        <h2>Main content coming...</h2>
+                    </Col>
+                    <Col lg={3}>
+                        <RightNav />
+                    </Col>
+                </Row>
+            </Container>
+            <Footer />
+        </div>
+    );
+};
+
+export default Main;
