2fbeb18d91ce06da53ab2f7ac6d06d223c07ae9e
ahad1033 <a.ahad2232@gmail.com>
2023-04-30 03:28:14 +0600

    react-icons-added

--- src/pages/shared/RightNav/RightNav.jsx
+++ src/pages/shared/RightNav/RightNav.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { FaGoogle, FaGithub} from 'react-icons/fa';
 
 const RightNav = () => {
     return (
         <div>
-            <h2>Right nav</h2>
+            <h4>Login with</h4>
+            <Button className='mb-2' variant="outline-primary"><FaGoogle /> Login with Google</Button>
+            <Button variant="outline-secondary"><FaGithub /> Login with Github</Button>
         </div>
     );
 };
