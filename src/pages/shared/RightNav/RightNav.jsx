7f9988fdd9050423267744036e49dcab517c729e
ahad1033 <a.ahad2232@gmail.com>
2023-04-30 03:34:40 +0600

    right-navigation-added

--- src/pages/shared/RightNav/RightNav.jsx
+++ src/pages/shared/RightNav/RightNav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
-import { FaGoogle, FaGithub} from 'react-icons/fa';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const RightNav = () => {
     return (
@@ -8,6 +8,14 @@ const RightNav = () => {
             <h4>Login with</h4>
             <Button className='mb-2' variant="outline-primary"><FaGoogle /> Login with Google</Button>
             <Button variant="outline-secondary"><FaGithub /> Login with Github</Button>
+            <div>
+                <h4>Find us on</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook /> facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter /> twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram /> instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
         </div>
     );
 };
