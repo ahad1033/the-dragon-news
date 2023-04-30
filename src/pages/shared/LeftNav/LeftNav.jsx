9f50c8068aa370c6778833c09e6c646ab80226ea
ahad1033 <a.ahad2232@gmail.com>
2023-05-01 02:52:41 +0600

    62-7

--- src/pages/shared/LeftNav/LeftNav.jsx
+++ src/pages/shared/LeftNav/LeftNav.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LeftNav = () => {
+
+    const [catagories, setCatagories] = useState([]);
+
+    useEffect( () => {
+        fetch('http://localhost:5000/catagories')
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error))
+    }, [])
+
     return (
         <div>
             <h2>Left nav</h2>
