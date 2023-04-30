898e7d817010672db13ef36c6cf6b762bbcb2124
ahad1033 <a.ahad2232@gmail.com>
2023-05-01 03:10:15 +0600

    62-8

--- src/pages/shared/LeftNav/LeftNav.jsx
+++ src/pages/shared/LeftNav/LeftNav.jsx
@@ -1,19 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
 
-    const [catagories, setCatagories] = useState([]);
+    const [categories, setCategories] = useState([]);
 
-    useEffect( () => {
+    useEffect(() => {
         fetch('http://localhost:5000/catagories')
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(error => console.error(error))
+            .then(res => res.json())
+            .then(data => setCategories(data))
+            .catch(error => console.error(error))
     }, [])
 
     return (
         <div>
-            <h2>Left nav</h2>
+            <h4>All category</h4>
+            <div className='ps-4 '>
+                {
+                    categories.map(category => <p
+                        key={category.id}
+                    >
+                        <Link to={`/category/${category.id}`} className='text-black text-decoration-none' >{category.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
